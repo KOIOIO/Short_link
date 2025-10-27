f736a0eb4fb0aafba994635f37b7a83e876620a7
wang-wenyu-fdhfj <2652777599@qq.com>
2025-10-27 19:01:04 +0800

    添加登录注册功能

--- zero_remake/user-rpc/internal/logic/loginlogic.go
+++ zero_remake/user-rpc/internal/logic/loginlogic.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"example.com/shorturl/short-url/zero_remake/common/errmsg"
 	"example.com/shorturl/short-url/zero_remake/models"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 
 	"example.com/shorturl/short-url/zero_remake/user-rpc/internal/svc"
@@ -35,7 +36,16 @@ func (l *LoginLogic) Login(in *User.LoginRequest) (*User.LoginResponse, error) {
 			Message: errmsg.GetErrMsg(errmsg.ERROR_FAIL_TO_LOGIN),
 		}, nil
 	}
-	_, err := l.GetFromMysql(in.Password)
+
+	user, err := l.GetFromMysql(in.Username)
+	if err != nil {
+		return &User.LoginResponse{
+			Code:    errmsg.ERROR_FAIL_TO_LOGIN,
+			Message: errmsg.GetErrMsg(errmsg.ERROR_FAIL_TO_LOGIN),
+		}, nil
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(in.Password))
 	if err != nil {
 		return &User.LoginResponse{
 			Code:    errmsg.ERROR_FAIL_TO_LOGIN,
@@ -49,11 +59,17 @@ func (l *LoginLogic) Login(in *User.LoginRequest) (*User.LoginResponse, error) {
 	}, nil
 }
 
-func (l *LoginLogic) GetFromMysql(password string) (*models.Usermodel, error) {
+func (l *LoginLogic) GetFromMysql(username string) (*models.Usermodel, error) {
 	var user models.Usermodel
-	err := l.svcCtx.DB.Where("password=?", password).First(&user).Error
+	err := l.svcCtx.DB.Where("username=?", username).First(&user).Error
 	if err == gorm.ErrRecordNotFound {
 		return nil, errors.New("user not found")
 	}
 	return &user, nil
+}
+
+func (l *LoginLogic) checkPassword(hashedPassword, inputPassword string) bool {
+	// 比对哈希值和输入密码
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(inputPassword))
+	return err == nil // 无错误则匹配成功
 }
--- zero_remake/user-rpc/internal/logic/registerlogic.go
+++ zero_remake/user-rpc/internal/logic/registerlogic.go
@@ -5,10 +5,10 @@ import (
 	"errors"
 	"example.com/shorturl/short-url/zero_remake/common/errmsg"
 	"example.com/shorturl/short-url/zero_remake/models"
-	"gorm.io/gorm"
-
 	"example.com/shorturl/short-url/zero_remake/user-rpc/internal/svc"
 	"example.com/shorturl/short-url/zero_remake/user-rpc/types/User"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
 
 	"github.com/zeromicro/go-zero/core/logx"
 )
@@ -42,6 +42,7 @@ func (l *RegisterLogic) Register(in *User.RegisterRequest) (*User.RegisterRespon
 			Message: errmsg.GetErrMsg(errmsg.ERROR_FAIL_TO_REGISTER),
 		}, nil
 	}
+	//检查用户名是否存在
 	exist, err := l.CheckExist(in.Username)
 	if err != nil {
 		return &User.RegisterResponse{
@@ -55,9 +56,18 @@ func (l *RegisterLogic) Register(in *User.RegisterRequest) (*User.RegisterRespon
 			Message: errmsg.GetErrMsg(errmsg.ERROR_FAIL_TO_REGISTER),
 		}, nil
 	}
+
+	//密码加密
+	bycrtypassword, err := l.hashPassword(in.Password)
+	if err != nil {
+		return &User.RegisterResponse{
+			Code:    errmsg.ERROR_FAIL_TO_REGISTER,
+			Message: errmsg.GetErrMsg(errmsg.ERROR_FAIL_TO_REGISTER),
+		}, nil
+	}
 	userinfo := models.Usermodel{
 		Username: in.Username,
-		Password: in.Password,
+		Password: bycrtypassword,
 	}
 
 	err = l.SaveToDB(&userinfo)
@@ -80,12 +90,21 @@ func (l *RegisterLogic) SaveToDB(user *models.Usermodel) error {
 
 func (l *RegisterLogic) CheckExist(username string) (bool, error) {
 	var user models.Usermodel
+	// 查询数据库中是否存在该用户名
 	err := l.svcCtx.DB.Where("username = ?", username).First(&user).Error
 	if err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return false, nil
+			return false, nil // 用户名不存在
 		}
-		return false, err
+		return false, err // 其他数据库错误
 	}
-	return true, nil
+	return true, nil // 用户名存在
+}
+
+func (l *RegisterLogic) hashPassword(password string) (string, error) {
+	hashbytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hashbytes), nil
 }
