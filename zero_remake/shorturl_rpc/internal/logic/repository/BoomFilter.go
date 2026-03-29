da5788c6765ffccc6c97f143a7f845685624296d
wang-wenyu-fdhfj <2652777599@qq.com>
2026-03-29 14:55:28 +0800

    实现布隆过滤器渐进式扩容，每次读写操作迁移一半数据

--- zero_remake/shorturl_rpc/internal/logic/repository/BoomFilter.go
+++ zero_remake/shorturl_rpc/internal/logic/repository/BoomFilter.go
@@ -3,56 +3,104 @@ package repository
 import (
 	"crypto/md5"
 	"crypto/sha1"
+	"sync"
 )
 
 const bloomFilterSize = 100
 const numHashFunctions = 7
 
 type BloomFilter struct {
-	bitArray []bool
+	bitArray     []bool
+	newBitArray  []bool
+	migrateIndex int
+	migrating    bool
+	mu           sync.Mutex
 }
 
 // 返回布隆
 func NewBloomFilter() *BloomFilter {
 	return &BloomFilter{
-		bitArray: make([]bool, bloomFilterSize),
+		bitArray:     make([]bool, bloomFilterSize),
+		newBitArray:  nil,
+		migrateIndex: 0,
+		migrating:    false,
 	}
 }
 
 // Add 方法将给定的URL添加到布隆过滤器中。
 // 它通过计算多个哈希值并将相应的位设置为true来实现。
 func (bf *BloomFilter) Add(url string) {
-	// 若当前位数组已满（所有位为 true），则先扩容
-	if bf.isFull() {
-		bf.AddCapacity()
+	bf.mu.Lock()
+	if bf.migrating {
+		bf.incrementalMigrate()
 	}
 	hashes := bf.getHashIndices(url)
 	for _, index := range hashes {
 		bf.bitArray[index] = true
 	}
-	// 如果本次写入使位数组达到满载，则立即扩容，降低后续误判
 	if bf.isFull() {
-		bf.AddCapacity()
+		bf.startMigration()
+	}
+	bf.mu.Unlock()
+}
+
+func (bf *BloomFilter) startMigration() {
+	if bf.migrating {
+		return
+	}
+	newSize := len(bf.bitArray) * 2
+	bf.newBitArray = make([]bool, newSize)
+	bf.migrateIndex = 0
+	bf.migrating = true
+}
+
+func (bf *BloomFilter) incrementalMigrate() {
+	if !bf.migrating || bf.newBitArray == nil {
+		return
+	}
+	oldSize := len(bf.bitArray)
+	newSize := len(bf.newBitArray)
+	halfSize := oldSize / 2
+	endIndex := bf.migrateIndex + halfSize
+	if endIndex > oldSize {
+		endIndex = oldSize
+	}
+	for i := bf.migrateIndex; i < endIndex; i++ {
+		if bf.bitArray[i] {
+			newIndex := bf.rehashIndex(i, newSize)
+			bf.newBitArray[newIndex] = true
+		}
+	}
+	bf.migrateIndex = endIndex
+	if bf.migrateIndex >= oldSize {
+		bf.bitArray = bf.newBitArray
+		bf.newBitArray = nil
+		bf.migrating = false
+		bf.migrateIndex = 0
 	}
 }
 
-// 用于扩容bloomfliter
-func (bf *BloomFilter) AddCapacity() {
-	newBitArray := make([]bool, len(bf.bitArray)*2)
-	copy(newBitArray, bf.bitArray)
-	bf.bitArray = newBitArray
+func (bf *BloomFilter) rehashIndex(oldIndex int, newSize int) int {
+	hash := md5.Sum([]byte(string(rune(oldIndex))))
+	return int(hash[0])<<8 | int(hash[1])%newSize
 }
 
 // MightContain 方法检查布隆过滤器是否可能包含给定的URL。
 // 如果所有计算出的哈希位都为true，则返回true，表示可能包含。
 // 请注意，这个方法可能会产生假阳性，但不会产生假阴性。
 func (bf *BloomFilter) MightContain(url string) bool {
+	bf.mu.Lock()
+	if bf.migrating {
+		bf.incrementalMigrate()
+	}
 	hashes := bf.getHashIndices(url)
 	for _, index := range hashes {
 		if !bf.bitArray[index] {
+			bf.mu.Unlock()
 			return false
 		}
 	}
+	bf.mu.Unlock()
 	return true
 }
 
